3452a0ae66e5577c7c93f482175f72241670634b
Prateek Vachher <vachh007@umn.edu>
2018-02-18 03:21:45 -0600

    Updated IP

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
-broker_url = 'amqp://myuser:mypassword@128.101.53.68:5672/myvhost'
-backend_url = 'rpc://myuser:mypassword@128.101.53.68:5672/myvhost'
+broker_url = 'amqp://myuser:mypassword@54.69.82.183:5672/myvhost'
+backend_url = 'rpc://myuser:mypassword@54.69.82.183:5672/myvhost'
 app = Celery('tasks', broker=broker_url, backend = backend_url)
 
 @app.task
